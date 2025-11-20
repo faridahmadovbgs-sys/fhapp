3c8faff4d15f739c58712da5cb69d2aa4b7640ff
FH App Developer <developer@fhapp.com>
2025-11-20 10:16:37 -0600

    fix: Remove unused setShowEmojiPicker reference

--- client/src/pages/ChatPage.js
+++ client/src/pages/ChatPage.js
@@ -329,7 +329,6 @@ const ChatPage = () => {
         });
       }
 
-      setShowEmojiPicker(false);
       setReactionTarget(null);
     } catch (error) {
       console.error('Error adding reaction:', error);
